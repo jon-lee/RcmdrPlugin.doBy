aeb934aea4be520638053962b5ac2dde0c320fbc
Jon Lee <jlee253@uwo.ca>
2011-12-14 22:11:28 -0500

    0.1-1
    Fixed help dialogs
    Added a changelog
    Added URL to description

--- RcmdrPlugin.doBy/R/RcmdrPlugin.doBy.R
+++ RcmdrPlugin.doBy/R/RcmdrPlugin.doBy.R
@@ -86,7 +86,7 @@ summaryByGUI <- function(){
 		closeDialog()
 		tkfocus(CommanderWindow())
 	}
-	OKCancelHelp(help="summaryby")
+	OKCancelHelp(help="summaryBy")
 	tkgrid(labelRcmdr(dsnameFrame, text=gettextRcmdr("Name for resulting data frame (leave blank to just print):  ")), entryDsname)
 	tkgrid(dsnameFrame, sticky="w", columnspan=2)
 	tkgrid(getFrame(variablesBox), getFrame(byBox), sticky="nw")
@@ -173,7 +173,7 @@ orderByGUI <- function(){
 		closeDialog()
 		tkfocus(CommanderWindow())
 	}
-	OKCancelHelp(help="orderby")
+	OKCancelHelp(help="orderBy")
 	tkgrid(labelRcmdr(dsnameFrame, text=gettextRcmdr("Name for resulting data frame (leave blank to just print):  ")), entryDsname)
 	tkgrid(dsnameFrame, sticky="w", columnspan=3)
 	tkgrid(getFrame(order1Box), getFrame(order2Box), getFrame(order3Box), sticky="nw")
@@ -233,7 +233,7 @@ splitByGUI <- function(){
 		closeDialog()
 		tkfocus(CommanderWindow())
 	}
-	OKCancelHelp(help="splitby")
+	OKCancelHelp(help="splitBy")
 	tkgrid(labelRcmdr(dsnameFrame, text=gettextRcmdr("Name for resulting data frame (leave blank to just print):  ")), entryDsname)
 	tkgrid(dsnameFrame, sticky="w", columnspan=1)
 	tkgrid(getFrame(byBox), sticky="nw")
@@ -306,7 +306,7 @@ sampleByGUI <- function(){
 		closeDialog()
 		tkfocus(CommanderWindow())
 	}
-	OKCancelHelp(help="sampleby")
+	OKCancelHelp(help="sampleBy")
 	tkgrid(labelRcmdr(dsnameFrame, text=gettextRcmdr("Name for resulting data frame (leave blank to just print):  ")), entryDsname)
 	tkgrid(dsnameFrame, sticky="w", columnspan=2)
 	tkgrid(getFrame(byBox), sticky="w")
